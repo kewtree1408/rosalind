21c0e01b1c29cf640fc255719e89233253b9b98b
Victoria <me@vika.space>
2019-01-19 08:50:21 +0100

    Added RNA solution

--- dna/solution.py
+++ dna/solution.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-# Given: A DNA string s of length at most 1000 nt.
-# Return: Four integers (separated by spaces) counting the respective
-# number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
-
 from collections import defaultdict
 
 
