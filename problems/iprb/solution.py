a15c3bc71f171d56579ccd068e2018f71d47779c
Victoria <me@vika.space>
2019-01-20 14:11:46 +0100

    Solution for Mendel Low

---
+++ problems/iprb/solution.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import pathlib
+
+
+def get_prob_of_dominant(k, m, n):
+    # A - dominant factor
+    # a - recessive factor
+    # k - amount of organisms with AA factors (homozygous dominant)
+    # m - amount of organisms with Aa factors (heterozygous)
+    # n - amount of organisms with aa factors (homozygous recessive)
+    events = ['AA+Aa', 'AA+aa', 'Aa+aa', 'AA+AA', 'Aa+Aa', 'aa+aa']
+
+    # get the probability of dominant traits (set up Punnett square)
+    punnett_probabilities = {
+        'AA+Aa': 1,
+        'AA+aa': 1,
+        'Aa+aa': 1 / 2,
+        'AA+AA': 1,
+        'Aa+Aa': 3 / 4,
+        'aa+aa': 0,
+    }
+    event_propapilities = {}
+    totals = k + m + n
+
+    # Event: AA+Aa -> P(X=k, Y=m) + P(X=m, Y=k):
+    P_km = k / totals * m / (totals - 1)
+    P_mk = m / totals * k / (totals - 1)
+    event_propapilities['AA+Aa'] = P_km + P_mk
+
+    # Event: AA+aa -> P(X=k, Y=n) + P(X=n, Y=k):
+    P_kn = k / totals * n / (totals - 1)
+    P_nk = n / totals * k / (totals - 1)
+    event_propapilities['AA+aa'] = P_kn + P_nk
+
+    # Event: Aa+aa -> P(X=m, Y=n) +P(X=n, Y=m):
+    P_mn = m / totals * n / (totals - 1)
+    P_nm = n / totals * m / (totals - 1)
+    event_propapilities['Aa+aa'] = P_mn + P_nm
+
+    # Event: AA+AA -> P(X=k, Y=k):
+    P_kk = k / totals * (k - 1) / (totals - 1)
+    event_propapilities['AA+AA'] = P_kk
+
+    # Event: Aa+Aa -> P(X=m, Y=m):
+    P_mm = m / totals * (m - 1) / (totals - 1)
+    event_propapilities['Aa+Aa'] = P_mm
+
+    # Event: aa+aa -> P(X=n, Y=n) + P(X=n, Y=n) = 0 (will be * 0, so just don't use it)
+    event_propapilities['aa+aa'] = 0
+
+    total_probability = 0
+    for event in events:
+        total_probability += punnett_probabilities[event] * event_propapilities[event]
+    return round(total_probability, 5)
+
+
+def main():
+    in_data = ''
+    current_dir = pathlib.Path(__file__).parent
+    with open(f'{current_dir}/input.txt') as fp:
+        k, m, n = map(int, fp.read().split(' '))
+    print(get_prob_of_dominant(k, m, n))
+
+
+if __name__ == "__main__":
+    main()
---
+++ problems/iprb/tests.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+try:
+    from solution import get_prob_of_dominant
+except ImportError:
+    from .solution import get_prob_of_dominant
+
+import unittest
+
+
+class TestRabbitPairs(unittest.TestCase):
+    def test_sample(self):
+        k = 2
+        m = 2
+        n = 2
+        result = 0.78333
+        assert get_prob_of_dominant(k, m, n) == result
+
+    def test_all_from_k(self):
+        k = 2
+        m = 0
+        n = 0
+        result = 1
+        assert get_prob_of_dominant(k, m, n) == result
+
+    def test_all_from_m(self):
+        k = 0
+        m = 2
+        n = 0
+        result = 3 / 4
+        assert get_prob_of_dominant(k, m, n) == result
+
+    def test_all_from_n(self):
+        k = 0
+        m = 0
+        n = 2
+        result = 0
+        assert get_prob_of_dominant(k, m, n) == result
+
+    def test_1_from_k_1_from_m(self):
+        k = 1
+        m = 1
+        n = 0
+        result = 1
+        assert get_prob_of_dominant(k, m, n) == result
+
+    def test_1_from_k_1_from_n(self):
+        k = 1
+        m = 0
+        n = 1
+        result = 1
+        assert get_prob_of_dominant(k, m, n) == result
+
+    def test_1_from_m_1_from_n(self):
+        k = 0
+        m = 1
+        n = 1
+        result = 1 / 2
+        assert get_prob_of_dominant(k, m, n) == result
+
+    def test_real(self):
+        k = 26
+        m = 26
+        n = 1
+        result = 0.78529
+        assert get_prob_of_dominant(k, m, n) == result
+
+
+if __name__ == '__main__':
+    unittest.main()
